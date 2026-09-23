acba4c5290cfc7ad9827ae0bc4d38b470df924a0
nicolelayssa <nicolelayssa@outlook.com>
2026-09-23 12:29:37 -0300

    Create app3.py

---
+++ app3.py
@@ -0,0 +1,21 @@
+name = input("Qual seu nome? ")
+age = input("Qual sua idade? ")
+
+age = int(age)
+
+print(type(name))
+print(type(age))
+
+older = age + 10 
+print(f"{name} terá {older} daqui á 10 anos. ")
+
+
+
+
+
+print(int("10")) 
+print(type(int("10")))
+
+print(int(199.99))
+
+print(18.99 * 2)
